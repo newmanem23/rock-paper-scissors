cfb141929c100e7ac600af18a61635a5ed1fd51f
Ed Newman <newmanem23@gmail.com>
2023-06-28 18:02:32 -0400

    Add game function
    Added a game function that iterates over the round function and
    prompts user input for rock paper scissors selection

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const computerSelection = getComputerChoice();
-const playerSelection = "scissors";
-
 function getComputerChoice() {
     let i = Math.floor(Math.random() * 100) % 3;
     switch (i) {
@@ -35,9 +32,33 @@ function playRound(playerSelection, computerSelection) {
                 return "You lose, rock beats scissors!";
             }
             return "You win, scissors cuts paper";
+        }
+    }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = prompt("Choose rock, paper, or scissors: ").toLowerCase();
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        switch (result.charAt(4)) {
+            case "l":
+                computerScore++;
+                break;
+            case "w":
+                playerScore++;
+        }
+        console.log(`Computer: ${computerScore}\nPlayer: ${playerScore}`);
+    }
+    if (computerScore === playerScore) {
+        console.log("It's a tie!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer wins.");
+    } else {
+        console.log("You win!");
     }
 }
 
-
-
-
+game();
