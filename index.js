5a892003c245f6fd55fe7e198225ad467e71f192
Ed Newman <newmanem23@gmail.com>
2023-07-11 16:47:45 -0400

    Add checkGameWinner function

--- index.js
+++ index.js
@@ -10,25 +10,27 @@ function getComputerChoice() {
 function playRound(e) {
     let playerSelection = e.target.innerText;
     let computerSelection = getComputerChoice();
-    let winner = determineWinner(playerSelection, computerSelection);
+    let winner = determineRoundWinner(playerSelection, computerSelection);
     let message = document.querySelector('.message');
+
     switch(winner) {
         case playerSelection:
-            message.innerText = `You win!, ${playerSelection} beats ${computerSelection}`;
+            message.innerText = `You win! ${playerSelection} beats ${computerSelection}.`;
             playerScore++;
             break;
         case computerSelection:
-            message.innerText = `You lose!, ${computerSelection} beats ${playerSelection}`;
+            message.innerText = `You lose! ${computerSelection} beats ${playerSelection}.`;
             computerScore++;
             break;
         default: 
             message.innerText = `Both ${playerSelection}, it's a tie!`;
             break;
     }
-    updateScores(playerScore, computerScore);
+    updateScoreDisplay(playerScore, computerScore);
+    checkGameWinner();
 }
 
-function determineWinner(playerSelection, computerSelection) {
+function determineRoundWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) return "tie";
     switch (playerSelection){
         case "Rock":
@@ -43,12 +45,25 @@ function determineWinner(playerSelection, computerSelection) {
         }
 }
 
-function updateScores(playerScore, computerScore) {
+function updateScoreDisplay(playerScore, computerScore) {
     let scoreDisplay = document.querySelector('.score-container');
     scoreDisplay.innerText = `Player: ${playerScore}\nComputer: ${computerScore}`;
 }
 
+function checkGameWinner() {
+    if (computerScore !== 5 && playerScore !== 5) return;
+    let message = document.querySelector('.winner');
+    if (computerScore === 5) {
+        message.innerText = ' Computer wins!';
+    }
+    else {
+        message.innerText = ' You win!';
+    }
+    const buttons = document.querySelector('button');
+    buttons.forEach(button => button.setAttribute("disabled", ""));
+}
+
+let computerScore = 0;
+let playerScore = 0;
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
-let playerScore = 0;
-let computerScore = 0;
